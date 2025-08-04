3c661812048bbf32efdd0fcbdc60215910b8cb34
nielassis <nielvitorba@gmail.com>
2025-08-04 20:21:18 -0300

    chore: add drizzle setup with initial schema; add seed script

---
+++ src/db/index.ts
@@ -0,0 +1,8 @@
+// Make sure to install the 'pg' package
+import { drizzle } from "drizzle-orm/node-postgres";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL environment variable is not set");
+}
+
+export const db = drizzle(process.env.DATABASE_URL);
