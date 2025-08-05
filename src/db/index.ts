aa96ed403a874277acbf51682bb99997bd88946b
nielassis <nielvitorba@gmail.com>
2025-08-05 20:22:01 -0300

    chore: add better auth tables

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,11 @@
 // Make sure to install the 'pg' package
 import { drizzle } from "drizzle-orm/node-postgres";
+import * as schema from "./schema";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL environment variable is not set");
 }
 
-export const db = drizzle(process.env.DATABASE_URL);
+export const db = drizzle(process.env.DATABASE_URL, {
+  schema,
+});
