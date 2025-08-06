0a2cf2b7d64676958531284bbd1cb09acb585de1
nielassis <nielvitorba@gmail.com>
2025-08-06 18:07:41 -0300

    feat: improvements on ui and add new shadn components

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,23 +1,28 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import SignInForm from "./components/sign-in-form";
 import SignUpForm from "./components/sign-up-form";
+import Header from "@/components/common/header";
 
 const AuthenticationPage = () => {
   return (
-    <div className="flex w-full max-w-sm flex-col gap-6 p-5">
-      <Tabs defaultValue="signin">
-        <TabsList>
-          <TabsTrigger value="signin">Entrar</TabsTrigger>
-          <TabsTrigger value="siginup">Criar conta</TabsTrigger>
-        </TabsList>
-        <TabsContent value="signin">
-          <SignInForm />
-        </TabsContent>
-        <TabsContent value="siginup">
-          <SignUpForm />
-        </TabsContent>
-      </Tabs>
-    </div>
+    <>
+      <Header />
+
+      <div className="flex w-full flex-col gap-6 p-5">
+        <Tabs defaultValue="signin">
+          <TabsList>
+            <TabsTrigger value="signin">Entrar</TabsTrigger>
+            <TabsTrigger value="siginup">Criar conta</TabsTrigger>
+          </TabsList>
+          <TabsContent value="signin">
+            <SignInForm />
+          </TabsContent>
+          <TabsContent value="siginup">
+            <SignUpForm />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
   );
 };
 
