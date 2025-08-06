8a4414ba73503e8cd6cd757830c19a0e15c0d185
nielassis <nielvitorba@gmail.com>
2025-08-06 19:11:49 -0300

    feat: add footer and filter newly products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
 import CategorySelector from "@/components/common/category-selector";
+import Footer from "@/components/common/footer";
 import Header from "@/components/common/header";
 import ProductList from "@/components/common/product-list";
 import { Button } from "@/components/ui/button";
 import { db } from "@/db";
+import { productTable } from "@/db/schema";
+import { desc } from "drizzle-orm";
 import Image from "next/image";
 
 const Home = async () => {
   const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
+  const newlyCreatedProducts = await db.query.productTable.findMany({
+    orderBy: [desc(productTable.createdAt)],
     with: {
       variants: true,
     },
@@ -45,6 +55,9 @@ const Home = async () => {
             className="h-auto w-full"
           />
         </div>
+
+        <ProductList title="Novos produtos" products={newlyCreatedProducts} />
+        <Footer />
       </div>
     </>
   );
