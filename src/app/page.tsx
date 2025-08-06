ec39a19a72f28e7f485ea2eef01d251e8f7d7c29
nielassis <nielvitorba@gmail.com>
2025-08-06 18:57:27 -0300

    feat: add product list and product item compontent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,46 @@
 import Header from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
 import { Button } from "@/components/ui/button";
+import { db } from "@/db";
+import Image from "next/image";
 
-export default function Home() {
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
   return (
     <>
       <Header />
+
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner01.png"
+            alt="Leve uma vida com estilo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList title="Mais vendidos" products={products} />
+
+        <div className="px-5">
+          <Image
+            src="/banner02.png"
+            alt="Leve uma vida com estilo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
     </>
   );
-}
+};
+
+export default Home;
