3980206071ec1f1d604535398357b4c38f467d26
nielassis <nielvitorba@gmail.com>
2025-08-08 19:54:41 -0300

    feat: add cart summary

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -13,6 +13,9 @@ import { useQuery } from "@tanstack/react-query";
 import { getCart } from "@/actions/get-cart";
 import Image from "next/image";
 import CartItem from "./cart-item";
+import { ScrollArea } from "../ui/scroll-area";
+import { Separator } from "../ui/separator";
+import { formatCentsToBRL } from "@/helpers/money";
 
 const Cart = () => {
   const { data: cart, isPending: cartIsLoading } = useQuery({
@@ -33,19 +36,53 @@ const Cart = () => {
           <SheetTitle>Carrinho</SheetTitle>
         </SheetHeader>
 
-        <div className="space-y-4 px-5">
-          {cartIsLoading && <p>Carregando...</p>}
-          {cart?.items.map((item) => (
-            <CartItem
-              key={item.id}
-              id={item.id}
-              productName={item.productVariant.product.name}
-              productVariantName={item.productVariant.name}
-              productVariantImageUrl={item.productVariant.imageUrl}
-              productVariantPriceInCents={item.productVariant.priceInCents}
-              quantity={item.quantity}
-            />
-          ))}
+        <div className="flex h-full flex-col px-5 pb-5">
+          <div className="flex h-full max-h-full flex-col overflow-hidden">
+            <ScrollArea className="h-full">
+              <div className="flex h-full flex-col gap-8">
+                <div className="space-y-4 px-5">
+                  {cartIsLoading && <p>Carregando...</p>}
+                  {cart?.items.map((item) => (
+                    <CartItem
+                      key={item.id}
+                      id={item.id}
+                      productName={item.productVariant.product.name}
+                      productVariantName={item.productVariant.name}
+                      productVariantImageUrl={item.productVariant.imageUrl}
+                      productVariantPriceInCents={
+                        item.productVariant.priceInCents
+                      }
+                      quantity={item.quantity}
+                    />
+                  ))}
+                </div>
+              </div>
+            </ScrollArea>
+          </div>
+
+          {cart?.items.length && cart.items.length > 0 && (
+            <div className="flex flex-col gap-4">
+              <Separator />
+              <div className="items-cente flex justify-between text-xs">
+                <p>Subtotal: </p>
+                <p>{formatCentsToBRL(cart?.totalPriceInCents ?? 0)}</p>
+              </div>
+
+              <Separator />
+              <div className="items-cente flex justify-between text-xs">
+                <p>Entrega: </p>
+                <p className="text-primary">GRÁTIS</p>
+              </div>
+
+              <Separator />
+              <div className="items-cente flex justify-between text-xs">
+                <p>Total: </p>
+                <p>{formatCentsToBRL(cart?.totalPriceInCents ?? 0)}</p>
+              </div>
+
+              <Button className="mt-5 rounded-full">Finalizar compra</Button>
+            </div>
+          )}
         </div>
       </SheetContent>
     </Sheet>
